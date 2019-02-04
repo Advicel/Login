66b69cdab9f8cf6daffd0fe8180bbbbccf4138e7
Advicel <lsowetsl@gmai.com>
2019-02-04 17:40:28 +0300

    first commint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatTabsModule,
+  MatCardModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatButtonModule
+} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
